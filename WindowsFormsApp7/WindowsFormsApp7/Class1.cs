7cc4b1bb6c2a2451e254dbfcd746e580ed8fc79f
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2022-01-18 01:50:15 -0800

    Add files via upload

---
+++ WindowsFormsApp7/WindowsFormsApp7/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Class1
+    {
+        string compname;
+        string vidprob;
+        string nomprint;
+        string nazvprint;
+        string opisasnie;
+
+
+        //public string Compname { get => compname; set => compname = value; }
+       // public string Vidprob { get => vidprob; set => vidprob = value; }
+      //  public string Nomprint { get => nomprint; set => nomprint = value; }
+      //  public string Nazvprint { get => nazvprint; set => nazvprint = value; }
+       // public string Opisasnie { get => opisasnie; set => opisasnie = value; }
+        public string Compname { get => compname; set => compname = value; }
+        public string Vidprob { get => vidprob; set => vidprob = value; }
+        public string Nomprint { get => nomprint; set => nomprint = value; }
+        public string Nazvprint { get => nazvprint; set => nazvprint = value; }
+        public string Opisasnie { get => opisasnie; set => opisasnie = value; }
+    }
+}
---
+++ WindowsFormsApp7/WindowsFormsApp7/Class2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Domain;
+
+namespace Session
+{
+    public class Class2
+    {
+        SqlConnection conn;
+        SqlConnectionStringBuilder connectionStringBuilder;
+        void ConnectTo()
+        {
+            connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "DESKTOP-7VHVK81";
+            connectionStringBuilder.InitialCatalog = "cnpcaqt";
+            connectionStringBuilder.IntegratedSecurity = true;
+            conn = new SqlConnection(connectionStringBuilder.ToString());
+        }
+
+       // public void Insert(Class1 p)
+        //{
+        //  try
+          //  {
+            //    string cmdText = "INSERT INTO dbo.prob(vidprob, nomprint, nazvprint, opisasnie) VALUES(@vidprob, @nomprint, @nazvprint, @opisasnie)";
+              //  SqlCommand cmd = new SqlCommand(cmdText, conn);
+                //cmd.Parameters.AddWithValue("@vidprob", p.Vidprob);
+             //   cmd.Parameters.AddWithValue("@opisasnie", p.Opisasnie);
+               // conn.Open();
+            //    cmd.ExecuteNonQuery();
+           // }
+            //catch (Exception)
+            //{
+              //  ;
+            //}
+        }
+    }
+//} 
+
